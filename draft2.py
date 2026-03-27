50bf970d39d7718f4ce64249d99febb51eb6f9cf
giraudc - cpu <giraudcgc@gmail.com>
2026-03-27 11:12:58 +0100

    Started Exploratory Data Analysis (EDA)

--- draft2.py
+++ draft2.py
@@ -5,7 +5,7 @@ import copy
 
 
 """  
-# --- preparation ---
+========== Preparation =========== 
 url = "https://api.sgu.se/oppnadata/mineralrattigheter/ogc/features/v1/collections"
 
 response=requests.get(url)
@@ -24,23 +24,27 @@ else:
 
 """
 
-
 """ 
 # --- Before doing anything, let's check the structure ---
+# Transforms "data" into a str in JSON format
 print(json.dumps(data, indent=2))
 
 # note: no ID as INT but a str
 """
 
-
-
 """ 
-# Confirming IDs and titles as per above
+# --- Confirming IDs and titles as per above ---
+# :40 does that the ID column has always up to 40 characters
 for coll in data["collections"]:
     print(f"{coll['id']:40}  {coll['title']}")
 """
 
-# Now that I have chosen the collection, I have commented with # all the above
+
+"""
+========== The chosen collection ============
+
+Since I chose the collection, I commented all the above preparation
+"""
 
 collection = "ut-metaller-industrimineral-beviljade"   
 url = f"https://api.sgu.se/oppnadata/mineralrattigheter/ogc/features/v1/collections/{collection}/items"
@@ -62,52 +66,69 @@ else:
     print("Problem!") 
 
 
-# --- Convert to DataFrame ---
+# --- The raw data --- 
+print("----- Raw data -----")
 raw_data = response.json()
-print(f"==={raw_data}===")
+#print(f"{raw_data}")  *** uncomment to print data
 
 
-# --- data info ---
-print(f"===raw_data.keys()===")
 
-
-# --- backup ---
+# --- Backup ---
 raw_data_backup = copy.deepcopy(raw_data)
 
 
-# --- Access direct to the data ---
+# --- Creating Df, access to data ---
+print("------ DataFrame -----")
 df = pd.json_normalize(raw_data["features"])
-print(f"==={df}===")
+#print(f"{df}") *** uncomment to see df
 
-# --- removing columns for simplicity ---
-print(f"==={df.keys()}===")
+# --- Removing columns for simplicity ---
+print("----- All columns ------")
+print(f"{df.keys()}")
 
-
+print("----- Removed columns -----")
 removed_columns = ["type","geometry_name","geometry.type", "geometry.coordinates","properties.diarynr","properties.status",
-                  "properties.export_date", "properties.geom_area", "properties.geom_length"]
+"properties.export_date", "properties.geom_area", "properties.geom_length"]
+print(removed_columns)
 
 df_col_ok = df.drop(columns=removed_columns)
-print(f"==={df_col_ok}===")
+print("----- Kept columns ------")
+print(f"Kept columns ({len(df_col_ok.columns)}): {list(df_col_ok.columns)}")
 
 
-# --- List of the columns ---
-print(f"==={list(df_col_ok.columns)}===")
+
+'''
+========== Exploratory Data Analysis (EDA) ==========
+'''
+
+# --- Missing values ---
+print("----- Missing values -----")
+print(f"{df_col_ok.isna().sum()}")
 
 
-# --- missing values ---
-print(f"==={df_col_ok.isna().sum()}===")
+# --- Questions ----
+print("----- Counties with the most approvals -----")
+print(df_col_ok["properties.county"].value_counts().head(10))
 
 
-# --- QUESTIONS ----
-# Top 5 counties to get their permits approved
-print(df_col_ok["properties.county"].value_counts().head())
-
-
-# --- The 1rst mineral ---
-print("=== Top 5 ===")
+# --- The 1rst minerals ---
+print("----- Top 5 -----")
 print(df_col_ok["properties.mineral"].value_counts().head())
 
+# --- Mineral count ---
+print("----- Minerals count ----")
+mineral_count = (
+    df_col_ok["properties.mineral"]
+    .str.split(",")     # str becomes a list as ["gold", "silver", etc..]
+    .explode()          # separates in different rows --> 1 mineral per row
+    .str.strip()        # remove white spaces
+    .value_counts()     # count each mineral
+)
+print(mineral_count.head(13))
 
-# --- Permitting length Vs Countied Vs Minerals ---
 
+# --- Permitting length  ---
+# per counties?
+
+# --- Counties Vs Minerals ---
 
